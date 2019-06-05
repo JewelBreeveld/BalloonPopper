2e7c8483cf5f7da4d3346ea151159bc87f078e3b
JewelBreeveld <rjbreeveld@gmail.com>
2019-06-05 16:43:13 +0200

    ts compile setup serverside

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Balloon-Popper
+
+## Front-End
+
+## Back-End
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import "reflect-metadata";
 import { createKoaServer } from "routing-controllers";
-import Controller from "./controller";
+import MainController from "./game";
+import setupDb from "./db";
 
 const port = process.env.PORT || 4000;
 
 const app = createKoaServer({
-  controllers: [Controller]
+  cors: true,
+  controllers: [MainController]
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+setupDb()
+  .then(_ => app.listen(port, () => console.log(`Listening on port ${port}`)))
+  .catch(err => console.error(err));
